a35d3407647bd2d2b2b768a0fa88b130a0d12350
ntcreed <38397560+ntcreed@users.noreply.github.com>
2019-12-21 17:25:54 +0800

    Add files via upload

---
+++ precache-manifest.a3df23e1b0e9f9ab80d0b94b288194e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112c0b04d4c4eecb9152",
+    "url": "/css/app.cf5ca2da.css"
+  },
+  {
+    "revision": "cbba3d9dfa5129ded9cf6e321d193099",
+    "url": "/data.json"
+  },
+  {
+    "revision": "3566e2362178342d7ace6ff48ed78dbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "112c0b04d4c4eecb9152",
+    "url": "/js/app.ddc1a8c6.js"
+  },
+  {
+    "revision": "10126a893215ced7c166",
+    "url": "/js/chunk-vendors.153ec1c7.js"
+  },
+  {
+    "revision": "35096c001e535c7a02294b2dff24004e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
